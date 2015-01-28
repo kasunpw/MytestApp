52873f6f76d553dc649768d40d11819cd282db70
kasunpw <kasun.peththawadu@gmail.com>
2015-01-28 09:08:21 +0530

    changed db connection

--- AngularApp/MyScripts/Module.js
+++ AngularApp/MyScripts/Module.js
@@ -4,7 +4,6 @@
         return { value: 0 }
     });
 
-    //Defining Routing
     app.config(['$routeProvider','$locationProvider', function ($routeProvider,$locationProvider) {
         $routeProvider.when('/showemployees',
                             {
